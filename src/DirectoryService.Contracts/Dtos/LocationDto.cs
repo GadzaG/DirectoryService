95ef6f508ac303bbbd1f1e23957c0185b28a04d3
anrove <paperka.lip@gmail.com>
2025-09-13 20:52:09 +0300

    DS-B-6
    -я изначально работал с Result паттерном и кастомными Error'ами, поэтому я добавил только EndpointResult и Envelope<T>
    - чутка забегая вперед решил протестировать EndpointResult на QueryHandler'e
    - добавил правило для роутинга, чтобы все было в lowercase

---
+++ src/DirectoryService.Contracts/Dtos/LocationDto.cs
@@ -0,0 +1,3 @@
+﻿namespace DirectoryService.Contracts.Dtos;
+
+public record LocationDto(Guid Id, string Name,  AddressDto Address, string Timezone);
--- src/DirectoryService.Presentation/Controllers/LocationController.cs
+++ src/DirectoryService.Presentation/Controllers/LocationController.cs
@@ -1,25 +1,39 @@
-﻿using DirectoryService.Application.Locations.Commands.CreateLocation;
+﻿using DirectoryService.Application.Abstractions.Core;
+using DirectoryService.Application.Locations.Commands.CreateLocation;
+using DirectoryService.Application.Locations.Queries.GetLocations;
+using DirectoryService.Contracts.Dtos;
 using DirectoryService.Contracts.Requests.Locations;
 using DirectoryService.Domain.Shared;
+using DirectoryService.Presentation.EndpointResults;
 using DirectoryService.Presentation.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DirectoryService.Presentation.Controllers;
 
-
 [ApiController]
-[Route("api/locations")]
+[Route("api/[controller]")]
 public class LocationController(ILogger<LocationController> logger) : ControllerBase
 {
     [HttpPost]
-    public async Task<ActionResult<Guid>> Create(
+    public async Task<EndpointResult<Guid>> Create(
         [FromBody] CreateLocationRequest request,
         [FromServices] CreateLocationHandler handler,
         CancellationToken ct = default)
     {
         logger.LogInformation("Controller : createLocation");
-        var createLocationCommand = new CreateLocationCommand(request.LocationName, request.AddressDto, request.Timezone);
-        var result = await handler.Handle(createLocationCommand, ct);
-        return result.IsFailure ? result.Error.ToResponse() : Ok(Envelope.Ok(result.Value));
+        var createLocationCommand =
+            new CreateLocationCommand(request.LocationName, request.AddressDto, request.Timezone);
+        return await handler.Handle(createLocationCommand, ct);
+
+        // return result.IsFailure ? result.Error.ToResponse() : Ok(Envelope.Ok(result.Value));
+    }
+
+    [HttpGet]
+    public async Task<EndpointResult<List<LocationDto>>> GetAll(
+        [FromServices] GetLocationsHandler handler,
+        CancellationToken ct = default)
+    {
+        logger.LogInformation("Controller: GetAll");
+        return await handler.Handle(new GetLocationsQuery(), ct);
     }
 }
